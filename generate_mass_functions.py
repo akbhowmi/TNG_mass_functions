43038f37810b5d6f11beb79c8c5b8b8b36c925ab
Aklant Bhowmick <aklantbhowmick@holylogin04.rc.fas.harvard.edu>
2019-09-18 19:48:22 -0400

    update3

--- generate_mass_functions.py
+++ generate_mass_functions.py
@@ -2,7 +2,7 @@ import numpy
 import arepo_package
 
 Nbins = 15
-log_mass_min = 8
+log_mass_min = 6
 log_mass_max = 12
 
 basePath='/n/ghernquist/Illustris/Runs/Illustris-1/'
@@ -14,6 +14,6 @@ basePath='/n/hernquistfs3/IllustrisTNG/Runs/'+run+'/output/'
 for desired_redshift,col in zip([0.,3., 5.],['blue','red','green']):
     object_type='subhalo'
 #    basePath = '/ufrc/lblecha/aklantbhowmick/arepo_runs_aklant/L25_n128/output/'
-    category='stellar'
+    category='bh'
     centers,HMF,dHMF,output_redshift=arepo_package.get_mass_function(category,object_type,desired_redshift,basePath,Nbins,log_mass_min,log_mass_max,list_all=False)
     numpy.save('./'+run+'_'+object_type+'_'+category+'_z%.2f.npy'%desired_redshift,[centers,HMF,dHMF,output_redshift])
